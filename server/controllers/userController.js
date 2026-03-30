7b3e4456b530956d1e0d3eb5e6ab95c3598c535c
Hadi Raza <hadiraza.edu@gmail.com>
2026-03-30 14:58:09 +0500

    password length must be 6

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,6 +20,9 @@ export const registerUser = async (req, res) => {
     if (userExists) {
       return res.status(400).json({ message: 'User already exists' });
     }
+    if(password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters' });
+    }
     const user = new User({ name, email, password });
     await user.save();
     
